ae18d3ae61a705a855544d1a0fab6d89631976ae
eternal-moon <44058515+eternal-moon@users.noreply.github.com>
2020-01-06 13:53:44 +0545

    Update post_data.py

--- post_data.py
+++ post_data.py
@@ -8,10 +8,7 @@ app = Flask(__name__)
     data = {"ph_value": random.randint(0,14), 'turbidity':random.randint(0,5),'conductivity': random.randint(15, 80) }
     response = requests.post('http://127.0.0.1:5000/api/postdata', json=data)
     time.sleep(5)'''
-'''reuest le getpost easy banauxa... hareko data ko dict banako cha... post garyo (fist arg url, data)
-post api thavayena vane http libraay ma postdata?phvalueamp= get request handle garna lai get
-request.query.get
-.'''
+
 @app.route('/getData')
 def getData():
 		data={"ph_value":request.args.get('PH'), "conductivity":request.args.get('CONDUCT')}
